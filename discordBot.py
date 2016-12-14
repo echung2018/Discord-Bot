522d318d04fbbd80f9b08215d05edf0ff88b0f42
Elliot Chung <Elliot Chung>
2016-12-14 00:58:09 -0800

    added things

--- discordBot.py
+++ discordBot.py
@@ -1,33 +1,38 @@
 import discord
-import asyncio
 import os
 import random
+import urllib.request
+import re
+import asyncio
 
 client = discord.Client()
 
+ 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+	print('Up and running')
+
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith('!cat'):
+    if message.content.startswith('!cat'):
         dir = 'Data\Images\Cats'
         filename = random.choice(os.listdir(dir))
         await client.send_file(message.channel,'Data\Images\Cats\\' + filename)
+        print('working')
+        
+    elif message.content.startswith('!youtube'):
+        query = message.content[len('!youtube'):]
+        searchLink = 'https://www.youtube.com/results?search_query=' + query.replace(' ', '+')
+        html_content = urllib.request.urlopen(searchLink)
+        #search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+        
+        print(html_content.read().decode())
+        
+        
+        '''
+        link = ''
+        client.send_message(message.channel,  link)
+        '''
 
 client.run('MjU1NzI3MDQxMTg4NDYyNTky.CynueQ.G_p98nuLKEhuYXMipx2n1ZYYPwU')
